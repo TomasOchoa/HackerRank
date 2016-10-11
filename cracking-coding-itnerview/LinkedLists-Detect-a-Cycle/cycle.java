2c291d241cade20a251eb4ac70919a6c01c7a14b
Tomas A. Ochoa <ochoa.tom91@gmail.com>
2016-10-11 16:32:08 -0400

    Score 25/25
    Convert to JS

---
+++ cracking-coding-itnerview/LinkedLists-Detect-a-Cycle/cycle.java
@@ -0,0 +1,34 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+
+A Node is defined as: 
+    class Node {
+        int data;
+        Node next;
+    }
+*/
+
+boolean hasCycle(Node head) {
+    //Check if empty
+    if(head == null)
+        return false;
+    
+    //Setup pointers
+    Node curr = head;
+    Node next = head.next;
+    
+    //While curr is not next
+    while(curr != next){
+        //Cycle through the list
+        //if(next == null){
+        if(next == null || next.next == null){
+            return false;            
+        }        
+        curr = curr.next;
+        next = next.next.next;
+        
+    }
+    
+    //If it reaches here, curr was next, meaning a cycle, thus return true
+    return true;  
+}
